c3ba7e2a5a2921d4a03aa41934d7e8e39439b7de
Aleksei <98910189+09121988stroitel1@users.noreply.github.com>
2022-11-23 14:46:28 +0300

    solution kata Multiples of 3 or 5

---
+++ script.js
@@ -0,0 +1,11 @@
+//Multiples of 3 or 5
+function solution(number){
+let sum = 0;
+while(number > 0) {
+  number--
+   if(number % 3 === 0 || number % 5 === 0) {
+     sum += number
+  }
+}
+return sum
+}
