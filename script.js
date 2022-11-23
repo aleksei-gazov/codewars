d80030d630a7e40bfcb3811cf8e681caa16c5aa6
Aleksei <98910189+09121988stroitel1@users.noreply.github.com>
2022-11-23 16:52:07 +0300

    solution Length of missing array

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ while(number > 0) {
 }
 return sum
 }
+
+// Length of missing array
+
+function getLengthOfMissingArray(arrayOfArrays) {
+ const lengths = (arrayOfArrays || [])
+    .map(a => a ? a.length : 0)
+    .sort((a, b) => a - b)
+  
+  if (lengths.includes(0)) {
+    return 0
+  }
+
+  for (let i = 0; i < lengths.length - 1; i++) {
+    if (lengths[i] + 1 !== lengths[i + 1]) {
+      return lengths[i] + 1
+    }
+  }
+
+  return 0
+}
