93d4e798c3910f68b8408b27bc1c49ab37313c57
Aleksei <98910189+09121988stroitel1@users.noreply.github.com>
2023-01-11 11:42:53 +0300

    Solition Persistent Bugger.
    Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
    
    For example (Input --> Output):
    
    39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
    999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
    4 --> 0 (because 4 is already a one-digit number)

--- script.js
+++ script.js
@@ -112,6 +112,15 @@ function minMax(arr){
 }
 
 
+// Persistent Bugger.
+function persistence(num) {
+   let i = 0
+  while(num.toString().length !== 1){
+    num = num.toString().split("").reduce((a,b)=> a*b)
+    i++
+  }
+  return i
+}
 
 
 
