92e954d7af9e51141b66bb0e13e16d16015450a7
Aleksei <98910189+09121988stroitel1@users.noreply.github.com>
2023-01-11 11:43:38 +0300

    Solition Write Number in Expanded Form
    Write Number in Expanded Form
    You will be given a number and you will need to return it as a string in Expanded Form. For example:
    
    expandedForm(12); // Should return '10 + 2'
    expandedForm(42); // Should return '40 + 2'
    expandedForm(70304); // Should return '70000 + 300 + 4'
    NOTE: All numbers will be whole numbers greater than 0.

--- script.js
+++ script.js
@@ -123,6 +123,22 @@ function persistence(num) {
 }
 
 
+// Write Number in Expanded Form
+
+function expandedForm(num) {
+let multiple = 10;
+  let result = [];
+  while (num > 1){
+    let remainder = num%multiple;
+    if(remainder >0){
+      result.unshift(remainder);
+    }
+    num -= remainder;
+    multiple = multiple * 10;
+  }
+  return result.join(" + ");
+}
+
 
 
 
